000a640fab0f7ce5918b37a715be257b07e66156
pathVelisela <Ajaykumar.v@vbrsit.com>
2024-06-27 14:00:14 +0530

    Delete ui/src/Company Module directory

--- ui/src/Company Module/PayRoll/CompanySalaryStructure.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CompanySalaryStructure = () => {
-  return (
-    <div>CompanySalaryStructure</div>
-  )
-}
-
-export default CompanySalaryStructure
--- ui/src/Company Module/PayRoll/EmployeeSalaryList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const EmployeeSalaryList = () => {
-  return (
-    <div>EmployeeSalaryList</div>
-  )
-}
-
-export default EmployeeSalaryList
