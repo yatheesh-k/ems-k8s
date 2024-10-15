36128ccde6931fdc7c2fddbfff2522be0b4a94df
anushakpbt2023 <151521486+anushakpbt2023@users.noreply.github.com>
2024-10-15 19:17:08 +0530

    Deleted the Frontend and Backend Folders which is present inside the ems project directory (#207)

--- Backend/pay_slip2-test/ems/src/main/java/com/pbt/ems/repository/DashboardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pbt.ems.repository;
-
-import com.pbt.ems.entity.Dashboard;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DashboardRepository extends JpaRepository<Dashboard,Long> {
-}
