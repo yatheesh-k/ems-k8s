9dce0b100554c568a8179095d5fb251f9443f54b
anushakpbt2023 <151521486+anushakpbt2023@users.noreply.github.com>
2024-12-02 17:57:28 +0530

    resolved the offer letter validation issue (#361)

--- employee/src/main/java/com/pb/employee/request/OfferLetterRequest.java
+++ employee/src/main/java/com/pb/employee/request/OfferLetterRequest.java
@@ -26,12 +26,12 @@ public class OfferLetterRequest {
     private String referenceNo;
 
     @Schema(example = "employeeName")
-    @Pattern(regexp = "^(?:[A-Z]{2,}(?:\\s[A-Z][a-z]+)*|[A-Z][a-z]+(?:\\s[A-Z][a-z]+)*|[A-Z]+(?:\\s[A-Z]+)*)$", message = "{firstname.format}")
+    @Pattern(regexp = "^(?!\\s)(.*?)(?<!\\s)$", message = "{firstname.format}")
     @Size(min = 1, max = 100, message = "{firstName.size.message}")
     private String employeeName;
 
     @Schema(example = "employeeFatherName")
-    @Pattern(regexp = "^(?:[A-Z]{2,}(?:\\s[A-Z][a-z]+)*|[A-Z][a-z]+(?:\\s[A-Z][a-z]+)*|[A-Z]+(?:\\s[A-Z]+)*)$",
+    @Pattern(regexp = "^(?!\\s)(.*?)(?<!\\s)$",
             message = "{fathername.format}")
     @Size(min = 1, max = 100, message = "{firstName.size.message}")
     private String employeeFatherName;
