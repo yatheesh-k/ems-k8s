26138b6e9448efdfc56554138fc09b07d3763507
anushakpbt2023 <151521486+anushakpbt2023@users.noreply.github.com>
2024-12-26 18:32:11 +0530

    resolved the payslip bugs and added the bank apis (#487)

--- employee/src/main/java/com/pb/employee/controller/BankController.java
+++ employee/src/main/java/com/pb/employee/controller/BankController.java
@@ -3,6 +3,7 @@ package com.pb.employee.controller;
 
 import com.pb.employee.exception.EmployeeException;
 import com.pb.employee.request.BankRequest;
+import com.pb.employee.request.BankUpdateRequest;
 import com.pb.employee.request.DepartmentRequest;
 import com.pb.employee.request.DepartmentUpdateRequest;
 import com.pb.employee.service.BankService;
@@ -28,7 +29,7 @@ public class BankController {
     @Autowired
     private BankService bankService;
 
-    @RequestMapping(value = "/company/{companyId}/banks", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/company/{companyId}/bank", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
     @io.swagger.v3.oas.annotations.Operation(security = { @io.swagger.v3.oas.annotations.security.SecurityRequirement(name = Constants.AUTH_KEY) },
             summary = "${api.registerDepartment.tag}", description = "${api.registerDepartment.description}")
     @ResponseStatus(HttpStatus.CREATED)
@@ -40,6 +41,51 @@ public class BankController {
                                                 @RequestBody @Valid BankRequest bankRequest,
                                                 HttpServletRequest request) throws IOException,EmployeeException {
         return bankService.bankDetailsToCompany(companyId,bankRequest,request);
+    }
+
+    @RequestMapping(value = "/company/{companyId}/banks", method = RequestMethod.GET)
+    @io.swagger.v3.oas.annotations.Operation(security = { @io.swagger.v3.oas.annotations.security.SecurityRequirement(name = Constants.AUTH_KEY) },
+            summary = "${api.getBanks.tag}", description = "${api.getBanks.description}")
+    @ResponseStatus(HttpStatus.OK)
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description= "OK")
+    public ResponseEntity<?> getAllBanksByCompanyId(@Parameter(hidden = true, required = true, description = "${apiAuthToken.description}", example = "Bearer abcdef12-1234-1234-1234-abcdefabcdef")
+                                            @RequestHeader(Constants.AUTH_KEY) String authToken,
+                                            @PathVariable String companyId) throws EmployeeException, IOException {
+        return bankService.getAllBanksByCompanyId(companyId);
+    }
+
+    @RequestMapping(value = "/{companyName}/bank/{id}", method = RequestMethod.GET)
+    @io.swagger.v3.oas.annotations.Operation(security = { @io.swagger.v3.oas.annotations.security.SecurityRequirement(name = Constants.AUTH_KEY) },
+            summary = "${api.getBanks.tag}", description = "${api.getBanks.description}")
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description= "OK")
+    public ResponseEntity<?> getBankDetailsById(@Parameter(hidden = true, required = true, description = "${apiAuthToken.description}", example = "Bearer abcdef12-1234-1234-1234-abcdefabcdef")
+                                               @RequestHeader(Constants.AUTH_KEY) String authToken,
+                                               @PathVariable String companyName, @PathVariable String id) throws EmployeeException {
+        return bankService.getBankDetailsById(companyName, id);
+    }
+
+    @RequestMapping(value = "/company/{companyId}/bank/{bankId}", method = RequestMethod.PATCH,consumes = MediaType.APPLICATION_JSON_VALUE)
+    @io.swagger.v3.oas.annotations.Operation(security = { @io.swagger.v3.oas.annotations.security.SecurityRequirement(name = Constants.AUTH_KEY) },
+            summary = "${api.updateBank.tag}", description = "${api.updateBank.description}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "202", description= "Accepted")
+    public ResponseEntity<?> updateBankById(@Parameter(hidden = true, required = true, description = "${apiAuthToken.description}", example = "Bearer abcdef12-1234-1234-1234-abcdefabcdef")
+                                                  @RequestHeader(Constants.AUTH_KEY) String authToken,
+                                                  @PathVariable String companyId,
+                                                  @PathVariable String bankId,
+                                                  @RequestBody @Valid BankUpdateRequest bankUpdateRequest) throws EmployeeException,IOException {
+        return bankService.updateBankById(companyId,bankId, bankUpdateRequest);
+    }
+
+    @RequestMapping(value = "/{companyName}/bank/{bankId}", method = RequestMethod.DELETE)
+    @io.swagger.v3.oas.annotations.Operation(security = { @io.swagger.v3.oas.annotations.security.SecurityRequirement(name = Constants.AUTH_KEY) },
+            summary = "${api.deleteBank.tag}", description = "${api.deleteBank.description}")
+    @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description= "OK")
+    public ResponseEntity<?> deleteBankById(@Parameter(hidden = true, required = true, description = "${apiAuthToken.description}", example = "Bearer abcdef12-1234-1234-1234-abcdefabcdef")
+                                              @RequestHeader(Constants.AUTH_KEY) String authToken,
+                                              @PathVariable String companyName,
+                                              @PathVariable String bankId) throws EmployeeException {
+        return bankService.deleteBankById(companyName, bankId);
     }
     
 }
