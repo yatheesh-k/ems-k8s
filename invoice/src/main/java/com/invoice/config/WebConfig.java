48b0a9d82041a6514613b4423cf8fc340f325ed4
narasimhapbt <narasimha.k@vbrsit.com>
2025-03-06 16:05:00 +0530

    added new files for image issue

--- invoice/src/main/java/com/invoice/config/WebConfig.java
+++ invoice/src/main/java/com/invoice/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.invoice.config;
 
-import com.invoice.util.Constants;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // Maps "/assets/img/**" URL path to the "D:/ems/ui/public/assets/img/" directory
-        registry.addResourceHandler(Constants.IMAGE_PATH)
-                .addResourceLocations(Constants.IMAGE_PATH_FILE);
+        registry.addResourceHandler("/var/www/ems/assets/img/**")
+                .addResourceLocations("file:/var/www/ems/assets/img/");
     }
 }
---
+++ invoice/src/main/java/com/invoice/util/SSLUtil.java
@@ -0,0 +1,33 @@
+package com.invoice.util;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+
+
+public class SSLUtil {
+
+    public static void disableSSLVerification() throws Exception {
+        TrustManager[] trustAll = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    }
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAll, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+
+    }
+}
